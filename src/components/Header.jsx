9c60ffbe1fb6b040864a50b5ebf2560ed0b1c8b7
mamazoirovm <mirzarajimk@gmail.com>
2023-05-10 17:10:23 +0500

    Add text to site-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <header className='site-header'>
-
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum laborum numquam quod earum amet debitis accusantium ea velit tempora et odit culpa quisquam dolorum corporis sint dolorem iure impedit perspiciatis, cum provident possimus vitae aut reiciendis! Natus, numquam inventore. Nemo corporis asperiores ipsam provident harum dolorum repellendus aperiam enim nesciunt! Praesentium recusandae perspiciatis amet? Magni laborum deleniti quibusdam magnam sit doloremque ut architecto saepe dolore nisi ullam fugit vel unde recusandae, reprehenderit tempore. Doloremque odio quos unde explicabo nam modi ipsa vel eaque officiis magnam in maiores enim beatae similique, nihil voluptate placeat quia sunt at possimus pariatur, reprehenderit consequatur? Eligendi adipisci iste, enim porro ratione quia, doloribus itaque corporis impedit et optio fuga asperiores. Molestiae sit rerum, vitae hic sint eos soluta doloribus architecto voluptatibus nihil. Impedit aspernatur minus culpa, rem autem quia vero aliquam unde exercitationem delectus iusto magnam, dolor, vitae qui cumque corrupti cum. Non voluptatibus aut, nostrum et praesentium omnis dicta ullam, error voluptates qui sit molestias aliquam voluptatem illum laboriosam eius debitis? Unde adipisci reprehenderit consequuntur aspernatur fugiat odit magnam corrupti recusandae, ipsum, nisi exercitationem mollitia dolorum culpa distinctio? Soluta, delectus numquam iusto vel eum repellat sequi aliquam, quas magni fuga blanditiis fugiat, molestias voluptatibus?
     </header>
     )
   }
